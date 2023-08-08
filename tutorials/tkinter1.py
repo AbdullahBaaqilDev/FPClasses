aae4b43a0ac8e83bd51e14be196754e1325c91d7
AbdullahBaaqilDev <138492007+AbdullahBaaqilDev@users.noreply.github.com>
2023-08-08 12:33:12 +0300

    -

--- tutorials/tkinter1.py
+++ tutorials/tkinter1.py
@@ -7,14 +7,29 @@ window = Tk()
 # window.title("<window name>"")
 window.title("First Window")
 # size and position window.geometry(<width> x <hieght> + <x> + <y>)
-window.geometry("500x450+75+100")
+window.geometry("520x450+75+100")
+
+my_label = Label(
+    window,
+    text = "User Name: ",
+    font = (
+        # font name
+        "Arial",
+        # font size
+        25,
+        # style like (bold,italic,underline)
+        "bold"
+    )
+)
+
+my_label.grid(row = 0, column = 0)
 
 # Entry is like input
 name_entry = Entry(
     # first thing is the window it will show on
     window,
     # then inside here you can put your entry properties
-    width = 25,
+    width = 15,
     font = (
         # font name
         "Arial",
@@ -26,7 +41,31 @@ name_entry = Entry(
 )
 
 # just show it in med top of the screen
-name_entry.pack()
+name_entry.grid(row = 0,column = 1)
+
+def print_name():
+    label = Label(
+        window,
+        text = name_entry.get(),
+        font = (
+            "Arial",
+            20,
+            ""
+        )
+    )
+    label.grid(row=2,column=1)
+
+my_bubtton = Button(
+    window,
+    text = "print name",
+    command = print_name,
+    font = (
+        "Arial",
+        20,
+        ""
+    )
+)
+my_bubtton.grid(row = 1,column = 1)
 
 # mainloop is the run command to show the window
 window.mainloop()
